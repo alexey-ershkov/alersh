72d01f04426f7249c4012d38be26962a5b388b5d
Alexey Ershkov <a.ershkov@corp.mail.ru>
2021-11-30 11:23:19 +0300

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import './index.scss';
+
 import React from 'react';
 import styled from 'styled-components';
-import './index.scss';
 
 const StyledDiv = styled.div`
   color: red;
 `;
 
 const App = () => {
-    return <StyledDiv className="test"> react app ts </StyledDiv>;
+  return <StyledDiv className="test"> react app ts </StyledDiv>;
 };
 
 export default App;
